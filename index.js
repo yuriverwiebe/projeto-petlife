2dd79187937d584b517309332aa8d6600c107676
Yuri Verwiebe <156673468+yuriverwiebe@users.noreply.github.com>
2024-01-25 08:24:08 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida ){
+    duvida.addEventListener('click', function (){
+        duvida.classList.toggle('ativa')
+    })
+})
+
+//function nome(argumento) {
+    // conteúdo da função
+//}
